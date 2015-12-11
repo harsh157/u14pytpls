3bc4e4252c649e7b36360a0c029adee3543f916c
chetantarale <chetan_tarale@yahoo.com>
2015-12-11 13:42:50 +0530

    added files

--- version/pypy.sh
+++ version/pypy.sh
@@ -1,22 +1,29 @@
-#!/bin/bash -e
+#!/bin/bash
 
-# Install pypy
-echo "================= Installing pypy ==================="
-sudo apt-get install -y pypy
+#install Pypy
+wget https://bitbucket.org/pypy/pypy/downloads/pypy-4.0.1-linux64.tar.bz2
+tar -xvf pypy-4.0.1-linux64.tar.bz2 -C /opt/
+cp -s /opt/pypy-4.0.1-linux64/bin/pypy /usr/bin/pypy
+mkdir /opt/pypy
+mkdir /opt/pypy/site-packages/
+cd /opt/pypy
 
-echo "================= Install virtualenv ==================="
-# Install virtualenv
-sudo wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O /tmp/ez_setup.py
-sudo pypy /tmp/ez_setup.py
-sudo easy_install pip
+#Install pip
+wget https://bootstrap.pypa.io/get-pip.py
+pypy get-pip.py
+
+#Install virtualenv
 pip install virtualenv
 virtualenv -p pypy $HOME/venv/pypy
 
-echo "================= Install pip packages ==================="
 # Install pip packages
 . $HOME/venv/pypy/bin/activate
 pip install pyopenssl ndg-httpsclient pyasn1
 pip install nose mock pytest coverage
-CFLAGS="-O0" pip install lxml
-deactivate
 
+# optional lxml package as lxml is not compatible with pypy
+#pip install -e git+git://github.com/aglyzov/lxml.git@cffi#egg=lxml-cffi
+
+#Default lxml throws error
+#CFLAGS="-O0" #STATIC_DEPS=true pip install lxml #throws error :disappointed:
+deactivate
